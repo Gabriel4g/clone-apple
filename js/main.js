3c6a91655cb9db2c32c50c5a787c5776f703ed0f
Gabriel4g <gabrielprogramador11@gmail.com>
2022-10-14 21:43:37 -0500

    add feature in nav

--- js/main.js
+++ js/main.js
@@ -13,3 +13,18 @@ function navScrool() {
 document.addEventListener("scroll", () => {
     navScrool()
 })
+
+const buttonSearch = document.querySelector("#search-icon");
+const buttoncloseSearch = document.querySelector("#close-search");
+const contentNav = document.querySelector(".content-nav");
+const contentSearch = document.querySelector("#search-nav");
+
+buttonSearch.onclick = () => {
+    contentNav.style.display = "none";
+    contentSearch.style.display = "flex"
+}
+
+buttoncloseSearch.onclick = () => {
+    contentSearch.style.display = "none";
+    contentNav.style.display = "flex"
+}
